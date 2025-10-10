20263e78de3833cf5a4f3f52b2dd2585ed311028
iahta <153889016+iahta@users.noreply.github.com>
2025-10-10 13:18:12 -0500

    words stemmed

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,22 +1,48 @@
 from .search_utils import (
     DEFAULT_SEARCH_LIMIT,
     load_movies,
+    load_stop_words,
 )
+from nltk.stem import PorterStemmer
 import string
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
     for movie in movies:
-        processed_query = process_text(query)
-        processed_title = process_text(movie["title"])
-        if any(word in processed_title for word in processed_query):
+        query_tokens = tokenize_text(query)
+        title_tokens = tokenize_text(movie["title"])
+        if has_matching_token(query_tokens, title_tokens):
             results.append(movie)
             if len(results) >= limit:
                 break
     return results
 
 def process_text(text: str) -> list[str]:
-    text = text.translate(str.maketrans("", "", string.punctuation))
     text = text.lower()
-    text = [word for word in text.split() if word.strip()]
+    text = text.translate(str.maketrans("", "", string.punctuation))
     return text
+
+def has_matching_token(query_tokens: list[str],title_tokens: list[str]) -> bool:
+    for query_token in query_tokens:
+        for title_token in title_tokens:
+            if query_token in title_token:
+                return True
+    return False
+
+def tokenize_text(text: str) -> list[str]:
+    text = process_text(text)
+    tokens = text.split()
+    valid_tokens = []
+    for token in tokens:
+        if token:
+            valid_tokens.append(token)
+    stop_words = load_stop_words()
+    filtered_words = []
+    for word in valid_tokens:
+        if word not in stop_words:
+            filtered_words.append(word)
+    stemmer = PorterStemmer()
+    stemmed_words = []
+    for word in filtered_words:
+        stemmed_words.append(stemmer.stem(word))
+    return stemmed_words
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -5,12 +5,13 @@ DEFAULT_SEARCH_LIMIT = 5
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
+STOP_WORDS_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
 
 def load_movies() -> list[dict]:
     with open(DATA_PATH, "r") as f:
         data = json.load(f)
     return data["movies"]
 
-def process_text(text: str) -> str:
-    text = text.lower()
-    return text
+def load_stop_words() -> list[str]:
+    with open(STOP_WORDS_PATH, "r") as f: 
+        return f.read().splitlines()
