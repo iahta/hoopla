3ed549d3b3ce6390534debdd61480071c15d8ec0
iahta <153889016+iahta@users.noreply.github.com>
2025-10-10 14:03:36 -0500

    inverted index

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
-from lib.keyword_search import search_command
+from lib.keyword_search import (
+    search_command, 
+    load_movies,
+    InvertedIndex,
+)
 
 import argparse
+
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    build_command = subparsers.add_parser("build", help="Build Movie Database")
 
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
+
 
     args = parser.parse_args()
 
@@ -19,9 +26,15 @@ def main() -> None:
             results = search_command(args.query)
             for i, res in enumerate(results, 1):
                 print(f"{i}. {res['title']}")
+        case "build":
+            movies = load_movies()
+            inverted_index = InvertedIndex()
+            inverted_index.build(movies)
+            inverted_index.save()
+            docs = inverted_index.get_documents("merida")
+            print(f"First document for token 'merida' = {docs[0]}")
         case _:
             parser.print_help()
-
 
 if __name__ == "__main__":
     main()
--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -4,7 +4,9 @@ from .search_utils import (
     load_stop_words,
 )
 from nltk.stem import PorterStemmer
+import os
 import string
+import pickle
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
@@ -46,3 +48,28 @@ def tokenize_text(text: str) -> list[str]:
     for word in filtered_words:
         stemmed_words.append(stemmer.stem(word))
     return stemmed_words
+
+class InvertedIndex:
+    def __init__(self):
+        self.index = {} #token (strings) -set of doc ids
+        self.docmap = {} #doc ID (int) - full document object
+    def __add_document(self, doc_id: int, text: str):
+        tokenized_text = tokenize_text(text)
+        for token in tokenized_text:
+            if token not in self.index:
+                self.index[token] = set()
+            self.index[token].add(doc_id)
+    def get_documents(self, term: str):
+        term = term.lower()
+        doc_ids = self.index.get(term, set())
+        return sorted(doc_ids)
+    def build(self, movies: list[dict]):
+        for movie in movies:
+            self.__add_document(movie['id'], f"{movie['title']}{movie['description']}")
+            self.docmap[movie["id"]] = movie
+    def save(self):
+        os.makedirs("cache", exist_ok=True)
+        with open("cache/index.pkl", "wb") as f_index:
+            pickle.dump(self.index, f_index)
+        with open("cache/docmap.pkl", "wb") as f_docmap:
+            pickle.dump(self.docmap, f_docmap)
