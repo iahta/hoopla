757d0766204c532d7d6a4989abb3de64ae156fa5
iahta <153889016+iahta@users.noreply.github.com>
2025-10-26 18:46:42 -0500

    multimodal image search

--- cli/lib/multimodal_search.py
+++ cli/lib/multimodal_search.py
@@ -1,16 +1,45 @@
 from PIL import Image
 from sentence_transformers import SentenceTransformer
+from .semantic_search import cosine_similarity
+from .search_utils import load_movies
 
 class MultimodalSearch:
-    def __init__(self, model_name="clip-ViT-B-32"):
+    def __init__(self, documents: list[dict], model_name="clip-ViT-B-32"):
         self.model = SentenceTransformer(model_name)
+        self.documents = documents
+        self.texts = []
+        for doc in documents:
+            self.texts.append(f"{doc['title']}: {doc['description']}")
+        self.text_embeddings = self.model.encode(self.texts, show_progress_bar=True)
+
 
     def embed_image(self, path: str):
         img = Image.open(path)
         encode = self.model.encode([img])
         return encode[0]
     
+    def search_with_image(self, path: str):
+        image_embedding = self.embed_image(path)
+        result = []
+        for i, text_embedding in enumerate(self.text_embeddings):
+            cos_sim = cosine_similarity(image_embedding, text_embedding)
+            result.append({
+                "doc_id": self.documents[i]["id"],
+                "title": self.documents[i]["title"],
+                "description": self.documents[i]["description"],
+                "score": cos_sim
+            })
+        sorted_doc = sorted(result, key=lambda item: item['score'], reverse=True)[:5]
+        return sorted_doc
+
+    
 def verify_image_embedding(path: str):
     search = MultimodalSearch()
     embedding = search.embed_image(path)
     print(f"Embedding shape: {embedding.shape[0]} dimensions")
+
+def image_search_command(path: str):
+    movies = load_movies()
+    search = MultimodalSearch(movies)
+    result = search.search_with_image(path)
+    return result
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -3,14 +3,18 @@ import argparse
 
 from lib.multimodal_search import (
     verify_image_embedding,
+    image_search_command,
 )
 
 def main():
     parser = argparse.ArgumentParser(description="Multimodal Serach CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    verify_parser = subparsers.add_parser("verify_image_embedding", help="Search movies using BM25")
+    verify_parser = subparsers.add_parser("verify_image_embedding", help="verify image embedding works")
     verify_parser.add_argument("path", type=str, help="Path of image to verify")
+
+    image_search_parser = subparsers.add_parser("image_search", help="Search movies using image")
+    image_search_parser.add_argument("path", type=str, help="Path of image to search")
     
 
     args = parser.parse_args()
@@ -18,10 +22,13 @@ def main():
     match args.command:
         case "verify_image_embedding":
             verify_image_embedding(args.path)
+        case "image_search":
+            results = image_search_command(args.path)
+            for i, res in enumerate(results):
+                print(f"{i+1}. {res['title']} (similarity: {res['score']:.3f})")
+                print(f"       {res['description'][:100]}")
         case _:
             parser.print_help()
-        
-
 
 
 
